b4ce9b04ef4a9b4d050e29244e1193c906214266
Bernie Shishaye <bernie86@gmail.com>
2018-04-22 19:09:46 -0400

    first commit

--- Trucking.Entities/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Trucking.Entities
-{
-    public class Class1
-    {
-    }
-}
---
+++ Trucking.Entities/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trucking.Entities.Models
+{
+    class Customer
+    {
+    }
+}
